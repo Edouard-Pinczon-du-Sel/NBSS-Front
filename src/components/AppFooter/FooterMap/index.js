87a7677bf7b94d12756547703ff1d6265d34abe9
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-07-20 21:44:23 +0200

    fix : bug to change branch

--- src/components/AppFooter/FooterMap/index.js
+++ src/components/AppFooter/FooterMap/index.js
@@ -5,15 +5,7 @@ import './styles.scss';
 // == Composant
 function FooterMap() {
   return (
-    <div className="map">
-      <div className="map__infos">
-        <h3 className="map__title">Adresse :</h3>
-        <h4 className="map__data">54 Avenue de Nantes</h4>
-        <h4 className="map__data">44116 Vieillevigne</h4>
-        <h4 className="map__data">France</h4>
-        <img className="map__img" src={img} alt="map" />
-      </div>
-    </div>
+    <div>Footer Map</div>
   );
 }
 
