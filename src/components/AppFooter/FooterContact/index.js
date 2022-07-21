2a34f56c1d03371879b3869c42d3585dd9cbb87e
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-07-21 23:42:49 +0200

    add : component contact in footer with styles

--- src/components/AppFooter/FooterContact/index.js
+++ src/components/AppFooter/FooterContact/index.js
@@ -5,7 +5,14 @@ import './styles.scss';
 // == Composant
 function FooterContact() {
   return (
-    <div>Footer Contact</div>
+    <div className="contact">
+      <h3 className="contact__title">Contact :</h3>
+      <h4 className="contact__label">Mobile :</h4>
+      <h4 className="contact__data">06 25 59 18 11</h4>
+      <p className="contact__data--infos">lundi au vendredi de 9h à 18h et samedi de 9h30 à 12h30</p>
+      <h4 className="contact__label">Mail :</h4>
+      <h4 className="contact__data">nbservicesetsoins@gmail.com</h4>
+    </div>
   );
 }
 
