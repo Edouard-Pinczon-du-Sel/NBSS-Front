3f8d87210b50f2eb1667d5e91107cd8615342b82
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-07-21 23:49:40 +0200

    add : component horaires in footer with styles

--- src/components/AppFooter/FooterHoraires/index.js
+++ src/components/AppFooter/FooterHoraires/index.js
@@ -5,7 +5,14 @@ import './styles.scss';
 // == Composant
 function FooterHoraires() {
   return (
-    <div>Footer Horaires</div>
+    <div className="horaires">
+      <h4 className="horaires__title">Notre bureau est ouvert sans rendez-vous les</h4>
+      <div className="horaires__container--data">
+        <div className="horaires__border" />
+        <h3 className="horaires__data">Mardi : 14:00 - 17:00</h3>
+        <h3 className="horaires__data">Samedi : 9h30 - 12:30</h3>
+      </div>
+    </div>
   );
 }
 
