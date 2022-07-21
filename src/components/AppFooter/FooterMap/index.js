7ae54fd6b045d990ca2bd1abc1a1f0cd9449932e
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-07-21 23:54:24 +0200

    add : component map in footer with styles

--- src/components/AppFooter/FooterMap/index.js
+++ src/components/AppFooter/FooterMap/index.js
@@ -1,6 +1,7 @@
 // == Import
 // import PropTypesLib from 'prop-types';
 import './styles.scss';
+import img from '../../../assets/images/map.png';
 
 // == Composant
 function FooterMap() {
@@ -11,7 +12,7 @@ function FooterMap() {
         <h4 className="map__data">54 Avenue de Nantes</h4>
         <h4 className="map__data">44116 Vieillevigne</h4>
         <h4 className="map__data">France</h4>
-        <img className="map__img" src="#" alt="map" />
+        <img className="map__img" src={img} alt="map" />
       </div>
     </div>
   );
