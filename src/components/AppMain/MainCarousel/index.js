f741914bf8476921ac40ce5396545b084bf76b9b
tonylapoche <you@example.com>
2022-07-22 15:13:45 +0200

    first commit, adding package nuka-carousel and first testing

--- src/components/AppMain/MainCarousel/index.js
+++ src/components/AppMain/MainCarousel/index.js
@@ -1,11 +1,18 @@
+/* eslint-disable jsx-a11y/alt-text */
 // == Import
 // import PropTypesLib from 'prop-types';
+import Carousel from 'nuka-carousel';
 import './styles.scss';
 
 // == Composant
 function MainCarousel() {
   return (
-    <div>Carousel</div>
+    <Carousel>
+      <img src="https://fakeimg.pl/250x100/" />
+      <img src="https://fakeimg.pl/250x100/" />
+      <img src="https://fakeimg.pl/250x100/" />
+      <img src="https://fakeimg.pl/250x100/" />
+    </Carousel>
   );
 }
 
