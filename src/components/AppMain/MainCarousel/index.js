65ab7dd0f9116461b51b655fdb8457fcaf12407a
tonylapoche <you@example.com>
2022-07-23 14:17:27 +0200

    carousel in place and functional, responsive mobile first and pc. still needs some refectorization but overall it works

--- src/components/AppMain/MainCarousel/index.js
+++ src/components/AppMain/MainCarousel/index.js
@@ -13,21 +13,32 @@ function MainCarousel() {
       wrapAround
       cellAlign="center"
       className="test_v2"
-      pagingDotsClassName="dots"
-      renderTopCenterControls={({ currentSlide }) => (
-        <div>Slide: {currentSlide}</div>
+      adaptiveHeight
+      speed={1500}
+      autoplayInterval={4000}
+      renderBottomCenterControls={({ currentSlide }) => (
+        <div className="info">
+          <span className="info--title"> nb services et soins</span>
+          <span className="info--content">est une entreprise de services à la personne opérant dans plusieurs domaines. N’hésitez pas à faire appel à nous en bas de besoin !</span>
+        </div>
       )}
-      renderCenterLeftControls={false}
-      renderCenterRightControls={false}
       defaultControlsConfig={{
         pagingDotsStyle: {
           fill: 'red',
         },
+        nextButtonClassName: 'sliderBtn sliderBtn--Next',
+        nextButtonText: '>',
+        prevButtonClassName: 'sliderBtn sliderBtn--Prev',
+        prevButtonText: '<',
       }}
     >
+      <img className="test" src="https://fakeimg.pl/800x300/" />
       <img className="test" src="https://fakeimg.pl/800x250/" />
-      <img className="test" src="https://fakeimg.pl/800x250/" />
-      <img className="test" src="https://fakeimg.pl/800x250/" />
+      <img className="test" src="https://fakeimg.pl/800x400/" />
+      <img className="test" src="https://fakeimg.pl/1000x500/" />
+      <img className="test" src="https://fakeimg.pl/600x400/" />
+      <img className="test" src="https://fakeimg.pl/900x500/" />
+      <img className="test" src="https://fakeimg.pl/800x400/" />
     </Carousel>
   );
 }
