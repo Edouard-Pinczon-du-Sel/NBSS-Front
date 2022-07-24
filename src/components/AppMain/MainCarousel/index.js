56bcc11a96862234e378d76e439ff80640de644e
tonylapoche <you@example.com>
2022-07-24 19:54:20 +0200

    simulation of data reception in format JSON for Carousel

--- src/components/AppMain/MainCarousel/index.js
+++ src/components/AppMain/MainCarousel/index.js
@@ -1,8 +1,39 @@
+/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/alt-text */
 // == Import
 // import PropTypesLib from 'prop-types';
 import Carousel from 'nuka-carousel';
 import './styles.scss';
+//? petit teste de mapping (simulation de reception de donnée en Json pour les image du carousel)
+const dataExempleImgCarousel = {
+  data: [
+    {
+      id: '1',
+      link: 'https://fakeimg.pl/800x300/',
+      alt: 'fakeimg 800x300',
+    },
+    {
+      id: '2',
+      link: 'https://fakeimg.pl/800x250/',
+      alt: 'fakeimg 800x250',
+    },
+    {
+      id: '3',
+      link: 'https://fakeimg.pl/800x400/',
+      alt: 'fakeimg 800x400',
+    },
+    {
+      id: '4',
+      link: 'https://fakeimg.pl/1000x500/',
+      alt: 'fakeimg 1000x500',
+    },
+    {
+      id: '5',
+      link: 'https://fakeimg.pl/900x500/',
+      alt: 'fakeimg 900x500',
+    },
+  ],
+};
 
 // == Composant
 function MainCarousel() {
@@ -32,13 +63,7 @@ function MainCarousel() {
         prevButtonText: '<',
       }}
     >
-      <img className="test" src="https://fakeimg.pl/800x300/" />
-      <img className="test" src="https://fakeimg.pl/800x250/" />
-      <img className="test" src="https://fakeimg.pl/800x400/" />
-      <img className="test" src="https://fakeimg.pl/1000x500/" />
-      <img className="test" src="https://fakeimg.pl/600x400/" />
-      <img className="test" src="https://fakeimg.pl/900x500/" />
-      <img className="test" src="https://fakeimg.pl/800x400/" />
+      {dataExempleImgCarousel.data.map((item) => <img key={item.id} className="test" src={item.link} alt={item.alt} />)}
     </Carousel>
   );
 }
