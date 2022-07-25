3f8aefb0ddc46640f8e81ace842564bd689746c7
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-07-25 10:32:39 +0200

    add : statics routes of navbar with BrowserRouter,Routes, Route and  NavLink

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,30 @@
 // == Import
 import 'src/styles/index.scss';
 import 'src/components/AppHeader/stylesMediaQueries.scss';
+import { Routes, Route } from 'react-router-dom';
 
 // == Composant
 import AppHeader from '../AppHeader';
 import AppMain from '../AppMain';
+// import ServicesPage from '../ServicesPage';
+// import EmploisPage from '../EmploisPage';
+// import ContactPage from '../ContactPage';
+
 import AppFooter from '../AppFooter';
 
 function App() {
   return (
     <div className="app">
       <AppHeader />
-      <AppMain />
+      <Routes>
+        <Route path="/" element={<AppMain />} />
+        {/* <Route path="/services" element={<SercicesPage />} /> */}
+        {/* <Route path="/emplois" element={<EmploisPage />} /> */}
+        {/* <Route path="/contact" element={<ContactPage />} /> */}
+        {/* <Route path="/mentions-legal" element={<mentionsLegalPage />} /> */}
+        {/* <Route path="/*" element={<Error />} /> */}
+        {/* <Route path="/error" element={<FormError />} /> */}
+      </Routes>
       <AppFooter />
     </div>
   );
