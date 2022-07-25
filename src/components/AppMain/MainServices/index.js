b6aa35b6dff0ea9e60b3aa7b8359cd85b3945ebc
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-07-26 00:46:38 +0200

    update : replace images of previews and test

--- src/components/AppMain/MainServices/index.js
+++ src/components/AppMain/MainServices/index.js
@@ -1,7 +1,7 @@
 // == Import
 // import PropTypesLib from 'prop-types';
 import './styles.scss';
-// import img from '../../../assets/images/7.jpg';
+import img from '../../../assets/images/7.jpg';
 import ServicesPreviews from './ServicesPreviews';
 
 const dataExempleHomeServices = {
@@ -9,25 +9,25 @@ const dataExempleHomeServices = {
     {
       id: '1',
       name: 'Aides ménagères',
-      link: 'https://fakeimg.pl/800x300/',
+      link: img,
       alt: 'fakeimg 800x300',
     },
     {
       id: '2',
       name: "Gardes d'enfants",
-      link: 'https://fakeimg.pl/800x250/',
+      link: img,
       alt: 'fakeimg 800x250',
     },
     {
       id: '3',
       name: 'Aides à la personne',
-      link: 'https://fakeimg.pl/800x400/',
+      link: img,
       alt: 'fakeimg 800x400',
     },
     {
       id: '4',
       name: 'Aides administrative après décès',
-      link: 'https://fakeimg.pl/1000x500/',
+      link: img,
       alt: 'fakeimg 1000x500',
     },
   ],
