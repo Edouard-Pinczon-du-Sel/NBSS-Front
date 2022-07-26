1450db3fe395e5fedcf88cac4d150565e1968639
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-07-26 10:15:47 +0200

    update : add folder data, import of the real images, update the map

--- src/components/AppMain/MainServices/index.js
+++ src/components/AppMain/MainServices/index.js
@@ -1,44 +1,16 @@
 // == Import
 // import PropTypesLib from 'prop-types';
 import './styles.scss';
-import img from '../../../assets/images/7.jpg';
-import ServicesPreviews from './ServicesPreviews';
+import data from '../../data/services';
 
-const dataExempleHomeServices = {
-  data: [
-    {
-      id: '1',
-      name: 'Aides ménagères',
-      link: img,
-      alt: 'fakeimg 800x300',
-    },
-    {
-      id: '2',
-      name: "Gardes d'enfants",
-      link: img,
-      alt: 'fakeimg 800x250',
-    },
-    {
-      id: '3',
-      name: 'Aides à la personne',
-      link: img,
-      alt: 'fakeimg 800x400',
-    },
-    {
-      id: '4',
-      name: 'Aides administrative après décès',
-      link: img,
-      alt: 'fakeimg 1000x500',
-    },
-  ],
-};
+import ServicesPreviews from './ServicesPreviews';
 
 // == Composant
 function MainServices() {
   return (
     <div className="services__list">
       {
-        dataExempleHomeServices.data.map((item) => (
+        data.map((item) => (
           <ServicesPreviews key={item.id} name={item.name} link={item.link} alt={item.alt} />
         ))
       }
---
+++ src/components/data/services.js
@@ -0,0 +1,33 @@
+import menageImg from '../../assets/images/7.jpg';
+import enfantsImg from '../../assets/images/enfants.jpg';
+import doucheImg from '../../assets/images/douche.jpg';
+import administratifImg from '../../assets/images/administratif.jpg';
+
+export default
+[
+  {
+    id: '1',
+    name: 'Aides ménagères',
+    link: menageImg,
+    alt: 'fakeimg 800x300',
+    description: "Les services ménagers proposent : une aide au ménage, au repassage, entretien du linge, rangement à domicile et netoyyage des vitres, aide à l'ouverture et à la fermeture des volets, arrosage plantes, réfection de list...",
+  },
+  {
+    id: '2',
+    name: "Gardes d'enfants",
+    link: enfantsImg,
+    alt: 'fakeimg 800x250',
+  },
+  {
+    id: '3',
+    name: 'Aides à la personne',
+    link: doucheImg,
+    alt: 'fakeimg 800x400',
+  },
+  {
+    id: '4',
+    name: 'Aides administrative après décès',
+    link: administratifImg,
+    alt: 'fakeimg 1000x500',
+  },
+];
