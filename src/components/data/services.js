482eacfd0a93a38ce773366ac47ef99c240efc43
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-07-26 10:35:31 +0200

    update : map on Service composent is all right with props

--- src/components/data/services.js
+++ src/components/data/services.js
@@ -10,24 +10,27 @@ export default
     name: 'Aides ménagères',
     link: menageImg,
     alt: 'fakeimg 800x300',
-    description: "Les services ménagers proposent : une aide au ménage, au repassage, entretien du linge, rangement à domicile et netoyyage des vitres, aide à l'ouverture et à la fermeture des volets, arrosage plantes, réfection de list...",
+    description: "Le service ménager propose :  Aide au ménage et repassage, Entretien du linge, Rangement à domicile, Nettoyage des vitres, Aide à l'ouverture et à la fermeture des volets, Arrosage plantes, Réfection de lit...",
   },
   {
     id: '2',
     name: "Gardes d'enfants",
     link: enfantsImg,
     alt: 'fakeimg 800x250',
+    description: "Le service de garde d'enfants propose : Préparation des repas, Aide à la prise de repas, Aide aux devoirs, Accompagnement pour le trajet vers la crèche l'école et/ou des activiés extra scolaire, Aide à l'habillage, Aide aux soins d'hygiène",
   },
   {
     id: '3',
     name: 'Aides à la personne',
     link: doucheImg,
     alt: 'fakeimg 800x400',
+    description: "Le service d'aide à la personne propose : Aide partielle ou totale à la toilette, Aide partielle ou totale à l’habillageAide aux transferts et aux déplacementsAide à la préparation et à la prise des repasAccompagnement lors des sortiesAide aux courses",
   },
   {
     id: '4',
     name: 'Aides administrative après décès',
     link: administratifImg,
     alt: 'fakeimg 1000x500',
+    description: 'Le service administratif après décès propose : Aide pour envoi de courrier pour envoi de mail et appel téléphonique',
   },
 ];
