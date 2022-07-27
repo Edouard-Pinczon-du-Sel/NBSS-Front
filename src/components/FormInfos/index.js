c7850dc925e3b072b3bc5d01344cab911915cfa6
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-07-27 18:17:30 +0200

    add : Form informations customer inProgress

---
+++ src/components/FormInfos/index.js
@@ -0,0 +1,19 @@
+// == Import
+// import PropTypesLib from 'prop-types';
+import './styles.scss';
+
+// == Composant
+function FormInfos() {
+  return (
+    <div className="form--infos">
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+    </div>
+  );
+}
+
+// == Export
+export default FormInfos;
