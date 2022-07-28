5443f3063450996ae588b3401e1fb03c0bb39aed
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-07-28 09:35:40 +0200

    update: add a real explications of details services

--- src/data/services.js
+++ src/data/services.js
@@ -11,7 +11,7 @@ export default
     link: menageImg,
     alt: 'fakeimg 800x300',
     description: "Le service ménager propose :  Aide au ménage et repassage, Entretien du linge, Rangement à domicile, Nettoyage des vitres, Aide à l'ouverture et à la fermeture des volets, Arrosage plantes, Réfection de lit...",
-    explications: 'service ménager test',
+    explications: "Le service à la personne consiste à aider une personne âgée ou en situation de handicap dans les actes de la vie quotidienne. Nous aidons la personne pour sa toilette, l’habillage, la mise des bas de contention. Nous pouvons l'aider pour se lever et se coucher ainsi que pour les transférer. Notre équipe est formée pour utiliser le lève personne, le verticalisateur et le disque de transfert. Nous nous occupons également des préparations de repas à votre domicile et de l'aide à la prise des repas. Nous pouvons également vous accompagner lors d'une sortie ou pour vos courses. Nos interventions sont personnalisées et nous nous adaptons à chaque situation.",
   },
   {
     id: '2',
@@ -19,7 +19,7 @@ export default
     link: enfantsImg,
     alt: 'fakeimg 800x250',
     description: "Le service de garde d'enfants propose : Préparation des repas, Aide à la prise de repas, Aide aux devoirs, Accompagnement pour le trajet vers la crèche l'école et/ou des activiés extra scolaire, Aide à l'habillage, Aide aux soins d'hygiène",
-    explications: 'service garde endants test',
+    explications: "Le service ménager consiste à entretenir votre maison comme si c'était vous qui le faisiez ! Nous utilisons votre matériel et vos produits. Nous pouvons nettoyer vos vitres, vos volets mais attention nous ne pouvons monter que sur un escabeau à 3 marches et pas plus haut ! Nous entretenons votre linge et s'occupons du repassage.",
 
   },
   {
@@ -28,7 +28,7 @@ export default
     link: doucheImg,
     alt: 'fakeimg 800x400',
     description: "Le service d'aide à la personne propose : Aide partielle ou totale à la toilette, Aide partielle ou totale à l’habillageAide aux transferts et aux déplacementsAide à la préparation et à la prise des repasAccompagnement lors des sortiesAide aux courses",
-    explications: 'service aide personne test',
+    explications: "Le service de garde d'enfant consiste à garder vos enfants en respectant votre mode de vie ! Nous pouvons préparer les repas, les biberons, s'occuper des soins d'hygiène (changer les couches, brossage de dents, aide à l'habillage …) Puis nous pouvons emmener ou aller chercher vos enfants à la crèche, à l'école ou aux activités extra-scolaires. Puis, il est possible que nous les accompagnons pour l'aide aux devoirs.",
 
   },
   {
@@ -37,7 +37,7 @@ export default
     link: administratifImg,
     alt: 'fakeimg 1000x500',
     description: 'Le service administratif après décès propose : Aide pour envoi de courrier pour envoi de mail et appel téléphonique',
-    explications: 'service administratif test',
+    explications: "Le pack décès consiste à rédiger et envoyer les courriers aux différents organismes pour annoncer le décès de votre proche. Bénédicte vous reçoit une première fois à l'agence pour prendre acte du décès et voir vos besoins de rédaction. Elle reste à votre écoute pour différentes questions. *Le pack décès est accessible pour tous et non réservé aux bénéficiaires de NB Services et Soins.",
 
   },
 ];
